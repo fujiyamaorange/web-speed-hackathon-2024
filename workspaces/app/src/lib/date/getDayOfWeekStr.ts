aceba0b7bcec49c3f19943d61be6be2bf548904b
fujiyamaorange <kaito.bakuman@gmail.com>
2024-03-24 01:07:16 +0900

    feat: quit to use canvas and moment

--- workspaces/app/src/lib/date/getDayOfWeekStr.ts
+++ workspaces/app/src/lib/date/getDayOfWeekStr.ts
@@ -1,10 +1,14 @@
-import type moment from 'moment-timezone';
+
 
 const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'] as const;
 
-export const getDayOfWeekStr = (date: moment.Moment) => {
-  const dayOfWeek = date.day();
-  const dayStr = days.at(dayOfWeek);
+export const getDayOfWeekStr = (date: Date) => {
+  if (!(date instanceof Date)) {
+    throw new Error('Input must be a Date object');
+  }
+
+  const dayOfWeek = date.getDay(); // DateオブジェクトのgetDayメソッドで曜日のインデックスを取得
+  const dayStr = days[dayOfWeek]; // 配列から対応する曜日の名前を取得
   if (dayStr == null) {
     throw new Error('dayOfWeek is invalid');
   }
