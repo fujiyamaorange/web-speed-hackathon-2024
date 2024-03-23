ce8a5237c1b68334fe5622a230805c4a56069c97
fujiyamaorange <kaito.bakuman@gmail.com>
2024-03-23 14:20:29 +0900

    fix: fix iamge

--- workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
+++ workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
@@ -1,7 +1,7 @@
 export const HeroImage: React.FC = () => {
   return (
     <div style={{ aspectRatio: '16 / 9', width: '100%' }}>
-      <img alt="Cyber TOON" src="./image.webp" style={{ display: 'inline-block', width: '100%' }} />
+      <img alt="Cyber TOON" src="/assets/image.webp" style={{ display: 'inline-block', width: '100%' }} />
     </div>
   );
 };
